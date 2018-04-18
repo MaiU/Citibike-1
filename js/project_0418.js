b6a3737d18257c7bcf5952821b6f067c5319c68c
paulpro90 <plp2114@columbia.edu>
2018-04-18 12:39:38 -0400

    Paul_basic setup

---
+++ js/project_0418.js
@@ -0,0 +1,52 @@
+// ***** Global variables ***** //
+
+var topY = 50;
+var bottomY = 500;
+var leftX = 50;
+var rightX = 700;
+var textLeft = 30;
+var buttonLabelsWeather = ['Cloudy', 'Rain', 'Fog', 'Snow'];
+var buttonLabelsTime = ['11 AM', '5 PM', '11 PM'];
+var buttonLabelsGender = ['Male', 'Female'];
+var buttonLabelsCustomer = ['Subscriber', 'Non-Subscriber'];
+var buttonLabelsDay = ['Weekday', 'Weekend'];
+var buttonStartX = 320;
+var buttonStartY = 15;
+var buttonLength = 120;
+var buttonHeight = 20;
+var buttonSpacing = 10;
+var selectedButton = 0;
+var topBudget = new p5.Table;
+var bottomBudget = new p5.Table;
+// ***** Global variables for Rider_Num ***** //
+var gender = -0.0000000000000325;
+var usertypeCustomer = 0.000000000000008081;
+var usertypeSubscriber = 0.00000000000003729;
+var birth.year = 0.000000000000000275
+var ElevAM = -0.000000000000024;
+var ElevPM = 0.000000000000905;
+var FivePM = 0.00000000000000368;
+var weekday.x = -0.0000000000001499;
+var Temp = -0.00000000000000262;
+var wind = 0.00000000000000507;
+var cloudy = 0.0000000000000695;
+var rain = -0.0000000000000138;
+var fog = -0.0000000000000213;
+var snow = -0.0000000000000667;
+
+// ***** Global variables for Trip_Time ***** //
+var genderTT = 2.33;
+var usertypeCustomerTT = -2.4;
+var usertypeSubscriberTT = -15.07;
+var birth.yearTT = -0.0348;
+var ElevAMTT = 0.654;
+var ElevPMTT = 0.573
+var FivePMTT = 1.163;
+var weekday.xTT = -0.667;
+var TempTT = 0.0168;
+var windTT = -0.002;
+var cloudyTT = 0.618;
+var rainTT = 0.0073;
+var fogTT = -0.168;
+var snowTT = -1.314;
+
